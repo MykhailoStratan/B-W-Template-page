3e835f2e2346f9548ff02c3502f30663c48a4d67
Michael Stratan <36572099+lordfon@users.noreply.github.com>
2018-10-14 13:14:24 +0300

    add hover changes on buttons and photos

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-
-});
+//$(document).ready(function() {
+//    
+//});
+//
+//function hover(element) {
+//    element.setAttribute('src', 'images/circle-active.png');
+//  }
+//  
+//  function unhover(element) {
+//    element.setAttribute('src', '..images/circle.png');
+//  }
